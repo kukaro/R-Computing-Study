7e95e3e3ead3f5326f9c32df035e47d515cdfcb2
kukaro <justkukaro@naver.com>
2021-03-23 06:04:16 +0900

    feat: set

---
+++ 97p-logical.R
@@ -0,0 +1,6 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: justk
+# Created on: 2021-03-23
+
+c(T, F, T) & c(2 == 2, 3 > 4, T)
---
+++ 98p-set-ops.R
@@ -0,0 +1,9 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: justk
+# Created on: 2021-03-23
+
+x <- c(1, 2, 5)
+5 %in% x
+7 %in% x
+
