05f9ee60eaa795d5974b4ecd659e12ed9c435093
kukaro <justkukaro@naver.com>
2021-04-13 20:39:23 +0900

    feat: What is this?

---
+++ 87p-example.R
@@ -0,0 +1,12 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: coeat
+# Created on: 2021/03/23
+
+vec <- c(5, 1, 6, 27, 7, 9, 14)
+#1
+vec[c(2, 3, 6)]
+#2
+vec2 <- vec[c(-2,-3)]
+#3
+vec[3]<-5
