ba09ad8495988473b0681e9bbb2b1635ff6b0e06
kukaro <justkukaro@naver.com>
2021-04-23 21:00:47 +0900

    feat: adding answer 5

--- attendance/others/num5.R
+++ attendance/others/num5.R
@@ -9,6 +9,5 @@ C <- c(71, 72, 70, 68, 74)
 y <- c(rep(A, 6 * 5), rep(B, 7 * 5), rep(C, 7 * 6))
 group <- rep(1:3, 5 * 6 * 7)
 group_df <- data.frame(y, group)
-
 group_df <- transform(group_df, group = factor(group))
 aov(y ~ group, data = group_df)
