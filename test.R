4776452b60c9dc69ac623b761f8194fb28f4617a
kukaro <justkukaro@naver.com>
2021-06-01 21:09:58 +0900

    feat:adding 0218-2nd

--- test.R
+++ test.R
@@ -1,8 +1,15 @@
-#Quantile-Quantile 그림: 자료가 특정 분포를 따르는지 확인하는 용도
-with(Cars93,
-     qqnorm(Turn.circle)) #Cars93 자료의 Turn.circle 변수가 정규분포를 따르는지 알아보자. normal Q-Q plot 그려라
-with(Cars93,
-     qqnorm(Turn.circle,
-            main='Q-Q plot of Turn circle \n (U-turn space, feet)')) #제목 설정. \n함수로 줄바꿈 설정
-with(Cars93,qqline(Turn.circle)) #직선 덮어쓰리기
-with(Cars93,qqline(Turn.circle, col="pink", lwd=0.5)) #선 색 설정, 굵기 설정
+f <- function (x, a) (x - a)^2
+xmin <- optimize(f, c(0, 1), tol = 0.0001, a = 1/3)
+xmin
+
+## See where the function is evaluated:
+optimize(function(x) x^2*(print(x)-1), lower = 0, upper = 10)
+
+## "wrong" solution with unlucky interval and piecewise constant f():
+f  <- function(x) ifelse(x > -1, ifelse(x < 4, exp(-1/abs(x - 1)), 10), 10)
+fp <- function(x) { print(x); f(x) }
+
+plot(f, -2,5, ylim = 0:1, col = 2)
+'aaa'
+optimize(fp, c(-4, 20))   # doesn't see the minimum
+optimize(fp, c(-7, 20))
