2fd66ed639db69bc05069124429917a0dfa08bdd
kukaro <justkukaro@naver.com>
2021-06-02 21:26:13 +0900

    feat: adding 2017-2nd

--- test.R
+++ test.R
@@ -1,15 +1,4 @@
-f <- function (x, a) (x - a)^2
-xmin <- optimize(f, c(0, 1), tol = 0.0001, a = 1/3)
-xmin
-
-## See where the function is evaluated:
-optimize(function(x) x^2*(print(x)-1), lower = 0, upper = 10)
-
-## "wrong" solution with unlucky interval and piecewise constant f():
-f  <- function(x) ifelse(x > -1, ifelse(x < 4, exp(-1/abs(x - 1)), 10), 10)
-fp <- function(x) { print(x); f(x) }
-
-plot(f, -2,5, ylim = 0:1, col = 2)
-'aaa'
-optimize(fp, c(-4, 20))   # doesn't see the minimum
-optimize(fp, c(-7, 20))
+B <- 3:6
+dim(B) <- c(2, 2)
+B
+sweep(B, 1, c(1, 2), '+')
